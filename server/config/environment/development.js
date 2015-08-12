a760401c794c4e9def08ea9ba9222733766cfeae
Mashhod Rastgar <mashhoodr@gmail.com>
2015-08-12 20:06:15 +0500

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/bigbrother-dev'
+  },
+
+  seedDB: true
+};

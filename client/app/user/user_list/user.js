5f0bfef589d76a62f0f2757e49d7e264d55bccdd
Abdul Qadir <abdulqadir@marketlytics.com>
2015-09-14 18:19:56 +0500

    refactor user folder structure

--- client/app/user/user_list/user.js
+++ client/app/user/user_list/user.js
@@ -4,7 +4,7 @@ angular.module('bigBrotherApp')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/users', {
-        templateUrl: 'app/user/user.html',
+        templateUrl: 'app/user/user_list/user.html',
         controller: 'UserCtrl'
       });
   });

1a916f3597b6211e001c01eb37f7152b2b902715
Abdul Qadir <abdulqadir@marketlytics.com>
2015-10-09 11:15:17 +0500

    fetch records for registered devices only

--- server/api/record/record.controller.js
+++ server/api/record/record.controller.js
@@ -8,7 +8,7 @@ var config = require('../../config/environment');
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
 
-var getFilteredQuery = function(queryObj, callback) {
+var getFilteredQuery = function(queryObj, devices, callback) {
 	var args = {};
 	if(typeof queryObj.filter === 'undefined') return callback(null, {});
 	var tokens = queryObj.filter.split(',');
@@ -28,18 +28,26 @@ var getFilteredQuery = function(queryObj, callback) {
 		}
 	}
 	if(typeof args.device !== 'undefined') {
-		Device.findById(args.device, function(err, device) {
-			if(err) return callback(err, null);
-			if(!device) return ({err: 'device not found'}, null);
-			query.mac = device.mac;
-			callback(null, query);
+		var device = devices.filter(function(device) {
+			return device._id.toString() === args.device;
 		});
+		if(!device) {
+			return callback({err: 'device not found'}, null);
+		}
+		query.mac = device.mac;
+		callback(null, query);
 	} else {
+		var macs = devices.map(function(device) {
+			return device.mac;
+		});
+		query.mac = {
+			$in: macs
+		};
 		callback(null, query);
 	}
 };
 
-var getIncludes = function(queryObj, records, callback) {
+var getIncludes = function(queryObj, records, devices, callback) {
 	var tokens = typeof queryObj.include !== 'undefined' ? queryObj.include.split(',') : [];
 
 	if(tokens.length === 0) {
@@ -49,82 +57,83 @@ var getIncludes = function(queryObj, records, callback) {
 	}
 
 	User.find({}, function(err, users) {
-		Device.find({}, function(err, devices) {
-			var ret = {
-				includes: {},
-				results: []
-			};
+		var ret = {
+			includes: {},
+			results: []
+		};
 
-			if(tokens.indexOf('users') >= 0) ret.includes.users = {};
-			if(tokens.indexOf('devices') >= 0) ret.includes.devices = {};
+		if(tokens.indexOf('users') >= 0) ret.includes.users = {};
+		if(tokens.indexOf('devices') >= 0) ret.includes.devices = {};
 
-			//append mac addresses to user devices array
-			var usersCpy = users.map(function(user) { 
-				var userCpy = user.toObject(); //convert mongoose object to json
-				userCpy.devices.forEach(function(userDevice) {
-					userDevice.mac = devices.filter(function(device) {
-						return device._id.toString() === userDevice.deviceId.toString();
-					})[0].mac;
-				});
-				return userCpy;
+		//append mac addresses to user devices array
+		var usersCpy = users.map(function(user) { 
+			var userCpy = user.toObject(); //convert mongoose object to json
+			userCpy.devices.forEach(function(userDevice) {
+				userDevice.mac = devices.filter(function(device) {
+					return device._id.toString() === userDevice.deviceId.toString();
+				})[0].mac;
 			});
+			return userCpy;
+		});
 
-			var devicesCpy = devices.map(function(device) {
-				return device.toObject();
-			});
-				
-			records.forEach(function(record) {
-				var recordCpy = record.toObject();
+		var devicesCpy = devices.map(function(device) {
+			return device.toObject();
+		});
+			
+		records.forEach(function(record) {
+			var recordCpy = record.toObject();
 
-				/*
-				 * check every user's device history 
-				 * find if there's a device having the same mac address as the record
-				 * if a user has the device with the same mac address in his history of devices
-				 * then check if record's time lies between the range of the time user owned that device
-				 * append the user id to the record if all the conditions are true
-				 */
-				if(tokens.indexOf('users') >= 0) {
-					var user = usersCpy.filter(function(user) {
-						return user.devices.filter(function(userDevice) {
-							var ret = userDevice.mac.toLowerCase() === record.mac.toLowerCase() && record.lastUpdated >= Math.round(userDevice.startedOn.getTime() / 1000);
-							if(typeof userDevice.endedOn !== 'undefined') {
-								ret = ret && record.lastUpdated <= Math.round(userDevice.endedOn.getTime() / 1000);
-							}
-							return ret;
-						}).length;
-					})[0];
-					if(typeof user !== 'undefined') {
-						ret.includes.users[user._id] = user;
-						recordCpy.user = user._id;
-					}
+			/*
+			 * check every user's device history 
+			 * find if there's a device having the same mac address as the record
+			 * if a user has the device with the same mac address in his history of devices
+			 * then check if record's time lies between the range of the time user owned that device
+			 * append the user id to the record if all the conditions are true
+			 */
+			if(tokens.indexOf('users') >= 0) {
+				var user = usersCpy.filter(function(user) {
+					return user.devices.filter(function(userDevice) {
+						var ret = userDevice.mac === record.mac && record.lastUpdated >= Math.round(userDevice.startedOn.getTime() / 1000);
+						if(typeof userDevice.endedOn !== 'undefined') {
+							ret = ret && record.lastUpdated <= Math.round(userDevice.endedOn.getTime() / 1000);
+						}
+						return ret;
+					}).length;
+				})[0];
+				if(typeof user !== 'undefined') {
+					ret.includes.users[user._id] = user;
+					recordCpy.user = user._id;
 				}
+			}
 
-				if(tokens.indexOf('devices') >= 0) {
-					var device = devicesCpy.filter(function(device) {
-						return device.mac === record.mac.toLowerCase();
-					})[0];
-					if(typeof device !== 'undefined') {
-						ret.includes.devices[device._id] = device;
-						recordCpy.device = device._id;
-					}
+			if(tokens.indexOf('devices') >= 0) {
+				var device = devicesCpy.filter(function(device) {
+					return device.mac === record.mac;
+				})[0];
+				if(typeof device !== 'undefined') {
+					ret.includes.devices[device._id] = device;
+					recordCpy.device = device._id;
 				}
+			}
 
-				ret.results.push(recordCpy);
-			});
+			ret.results.push(recordCpy);
+		});
 
-			callback(ret);
-		}); 
+		callback(ret);
 	});
 };
 
 exports.index = function(req, res) {
-	getFilteredQuery(req.query, function(err, query) {
+	Device.find({}, function(err, devices) {
 		if(err) return res.status(500).send(err);
-		Record.find(query, function(err, records) {
+		getFilteredQuery(req.query, devices, function(err, query) {
 			if(err) return res.status(500).send(err);
-			//append corresponding user id and device id to each record
-			getIncludes(req.query, records, function(records) {
-				res.status(200).json(records);
+			Record.find(query, function(err, records) {
+				if(err) return res.status(500).send(err);
+				//append corresponding user id and device id to each record
+				getIncludes(req.query, records, devices, function(records) {
+					res.status(200).json(records);
+				});
 			});
 		});
 	});
@@ -163,6 +172,7 @@ exports.saveRecords = function(req, res) {
 exports.saveLogs = function(req, res) {
 	if(typeof req.file !== "undefined" && req.file.fieldname === 'log') {
 		var directory = 'logs/';
+		//create logs directory if it doesn't exist
 		try {
 			fs.lstatSync(directory);
 		} catch(e) {
